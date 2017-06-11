39d656cf6893bffb0b7d5a0f3c3c11add8ec1768
kienle <kienle83@gmail.com>
2017-06-12 00:21:28 +0200

    sass

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # centerpage
+
+## Teaser on homepage
+  - Documentation: https://v4-alpha.getbootstrap.com/layout/grid/
+
+## Notification function
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
+
+var idleTime = 0;
+var timestamp = null;
+//timestamp = '2017-06-08T21:34:39.244341+00:00';
+//timestamp = '2017-06-09T21:34:39.244341+00:00';
+
+
 /**
- * Check the page is visible or hidden (tab is active or inactive)
+ * Check the page is visible or hidden (tab is active or inactive).
  */
-var vis = (function(){
+var vis = (function() {
     var stateKey, eventKey, keys = {
-        hidden: "visibilitychange",
-        webkitHidden: "webkitvisibilitychange",
-        mozHidden: "mozvisibilitychange",
-        msHidden: "msvisibilitychange"
+        hidden: 'visibilitychange',
+        webkitHidden: 'webkitvisibilitychange',
+        mozHidden: 'mozvisibilitychange',
+        msHidden: 'msvisibilitychange'
     };
     for (stateKey in keys) {
         if (stateKey in document) {
@@ -21,25 +28,25 @@ var vis = (function(){
 })();
 
 /**
- * Fetch data from json file
+ * Fetch data from json file.
  */
 function fetchData() {
     $.ajax( {
-        url: "https://raw.githubusercontent.com/kienle83/centerpage/master/data/data.json",
+        url: 'https://raw.githubusercontent.com/kienle83/centerpage/master/data/data.json',
         type: 'GET',
         success: function( data ) {
-            var parsed = JSON.parse(data);
-            //var timestamp = "2017-06-08T21:34:39.244341+00:00";
-            var timestamp = "2017-06-09T21:34:39.244341+00:00";
+            var parsedData = JSON.parse(data),
+                lastChange = parsedData.last_published_semantic;
 
-            if (parsed.last_published_semantic && timestamp !== parsed.last_published_semantic) {
-                //console.log(parsed.last_published_semantic);
+            console.log('fetched data: ' + lastChange);
+
+            if (lastChange && timestamp !== lastChange) {
                 showOverlay();
+                timestamp = lastChange;
             }
         },
         error: function() {
-            console.log("error");
-            //setTimeout();
+            console.log('error');
         }
     });
 }
@@ -49,21 +56,20 @@ function fetchData() {
  */
 function showOverlay() {
 
-    var $overlayWrapper = $("#overlay-wrapper");
+    var $overlayWrapper = $('#overlay-wrapper');
 
-    if ($overlayWrapper.not(":visible")) {
-        $( "#overlay-wrapper" ).dialog({
+    if ( ! $overlayWrapper.is(':visible') ) {
+        $( '#overlay-wrapper' ).dialog({
             resizable: false,
-            height: "auto",
+            height: 'auto',
             width: 400,
             modal: true,
             buttons: {
-                "Neu laden": function() {
+                'Neu laden': function() {
                     location.reload();
-                    //$( this ).dialog( "close" );
                 },
-                "Abbrechen": function() {
-                    $( this ).dialog( "close" );
+                'Abbrechen': function() {
+                    $( this ).dialog( 'close' );
                 }
             }
         });
@@ -71,21 +77,52 @@ function showOverlay() {
 
 }
 
-
 /**
- * Bind events depend on visible/hidden page
+ * Bind events depend on visible/hidden page.
  */
-vis(function(){
+vis(function() {
 
-    if (vis()) {
-        console.log("document is visible");
-        document.title = 'Visible';
-        //bindResetEvents();
-        fetchData();
+    if ( vis() ) {
+        console.log('document is visible');
+
+        // if overlay is visible but user click away and come back to page then we do not need to fetch data.
+        if ( ! $('#overlay-wrapper').is(':visible') ) {
+            fetchData();
+        }
+
     } else {
-        console.log("document is hidden");
-        document.title = 'Not visible';
-        //unbindResetEvents();
+        console.log('document is hidden');
     }
 
 });
+
+
+/**
+ * Check user idle on page, if user is idle after a period then we will fetch data.
+ */
+
+$(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(_timerIncrement, 6000); // 1 minute
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+});
+
+/**
+ * If user is idle longer than 5 minutes then data will be fetched.
+ */
+function _timerIncrement() {
+    idleTime = idleTime + 1;
+    // 5 minutes
+    if (idleTime > 5 && ! $('#overlay-wrapper').is(':visible')) {
+        fetchData();
+    }
+}
+
+
